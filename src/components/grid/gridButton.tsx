a2f690ebd25bd379c22a6ce043f08a977272131d
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-06-04 13:48:52 -0600

    expanded gridButton

--- src/components/grid/gridButton.tsx
+++ src/components/grid/gridButton.tsx
@@ -1,13 +1,24 @@
-import { ReactNode } from "react"
+import { ReactNode, useState } from 'react'
 
 type GridButtonProps = {
 	children?: ReactNode
 	key?: string
+	defaultValue?: number
 }
 
 const GridButton = function GridButton(props: GridButtonProps) {
 	const { key } = props
-	return <button className='bg-fourth-color w-20 h-20' key={key}>{props.children}</button>
+	const [value, setValue] = useState<number | null>(props.defaultValue || null)
+
+	const content = value || props.children
+
+	return (
+		<button
+			className='bg-fourth-color w-20 h-20'
+			key={key} >
+			{content}
+		</button>
+	)
 }
 
 export default GridButton
