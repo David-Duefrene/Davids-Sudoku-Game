a7af7eea0be467e0ce227eedf8022187fa6dc880
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-06-06 11:27:29 -0600

    added onClick event to button

--- src/components/grid/gridButton.tsx
+++ src/components/grid/gridButton.tsx
@@ -9,11 +9,13 @@ type GridButtonProps = {
 const GridButton = function GridButton(props: GridButtonProps) {
 	const { key } = props
 	const [value, setValue] = useState<number | null>(props.defaultValue || null)
+	const [isClicked, setIsClicked] = useState<boolean>(false)
 
 	const content = value || props.children
 
 	return (
 		<button
+			onClick={() => setIsClicked(!isClicked)}
 			className='bg-fourth-color w-20 h-20'
 			key={key} >
 			{content}
